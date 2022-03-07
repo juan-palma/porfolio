c9b82171f9782781ef08789446209092d798760f
Juan Palma <juan.palma@me.com>
2022-03-07 17:49:56 -0600

    animacion home hola terminada v1.0

--- assets/js/owner/main.js
+++ assets/js/owner/main.js
@@ -11,9 +11,56 @@ const el = idagl.elementos;
 
 
 // ::::::::::::::::: Funciones :::::::::::::::::
-function aniB1(data){
+
+function controlTimeLine1(accion){
+	if(accion == 'stop'){
+		if(el.hasOwnProperty('home_hola_parallax')){
+			el.home_hola_parallax.disable();
+		}
+		el.hhola5.style.animation = 'none';
+		el.hhola4.style.animation = 'none';
+		el.hhola3.style.animation = 'none';
+		el.hhola2.style.animation = 'none';
+		el.home_hola_box.style.display = 'none';
+	} else{
+		if(el.hasOwnProperty('home_hola_parallax')){
+			el.home_hola_parallax.enable();
+		}
+		el.home_hola_box.style.display = 'flex';
+		el.hhola5.style.animation = 'hola5 3s cubic-bezier(0.455, 0.03, 0.515, 0.955) 10ms infinite alternate';
+		el.hhola4.style.animation = 'animation: hola4 4.6s linear 10ms infinite';
+		el.hhola3.style.animation = 'animation: hola3 3s cubic-bezier(0.455, 0.03, 0.515, 0.955) 10ms infinite alternate';
+		el.hhola2.style.animation = 'animation: hola2 4.6s linear 10ms infinite';
+	}
+}
+function animarTimeline1(data){
 	//console.log(data.padrePdesplazado * 10);
-	el.timeline1.seek( data.padrePdesplazado * 10 );
+	el.timeline1.data = data;
+	el.timeline1.seek( data.padrePdesplazado * 120 );
+}
+function makeTimeline1(parametro){
+	let targetsHola1 = document.querySelectorAll('#home_hola_box .hhola .anibox');
+	targetsHola1 = [...targetsHola1];
+	targetsHola1.reverse();
+	el.timeline1 = anime.timeline({
+		easing: "linear",
+		autoplay: false,
+	}).add({
+		targets: targetsHola1,
+		duration: 2000,
+		opacity: [
+			{ value: 0, duration: 250, delay: anime.stagger([400, 1300]), easing: 'linear' }
+		],
+		scale: [
+			{ value: 8, duration: 2000, delay: anime.stagger([0, 320]), easing: 'linear' }
+		],
+		changeBegin: ()=>{
+			if(el.timeline1.data.sentido == "reverse"){controlTimeLine1('run');};
+		},
+		changeComplete: ()=>{
+			if(el.timeline1.data.sentido == "normal"){controlTimeLine1('stop');};
+		}
+	})
 }
 
 
@@ -95,37 +142,26 @@ function iniciar() {
 	el.fondo = document.getElementById('fondo_body');
 	el.fondo.style.overflow = "hidden";
 	el.loading = document.getElementById('loading');
+	el.home_hola_box = document.getElementById('home_hola_box');
 	el.boxTimeline1 = document.getElementById('timeline1');
+	el.hhola5 = document.querySelector('#hhola5 .anibox > img:first-child');
+	el.hhola4 = document.querySelector('#hhola4 .anibox > img:first-child');
+	el.hhola3 = document.querySelector('#hhola3 .anibox > img:first-child');
+	el.hhola2 = document.querySelector('#hhola2 .anibox > img:first-child');
 
-
-	let targetsHola1 = document.querySelectorAll('#home_hola_box .hhola .anibox');
-	targetsHola1 = [...targetsHola1];
-	targetsHola1.reverse();
-	el.timeline1 = anime.timeline({
-		easing: "linear",
-		autoplay: false,
-	}).add({
-		targets: targetsHola1,
-		opacity: [
-			{ value: 0, duration: 400, delay: 600, easing: 'easeInCubic' }
-		],
-		scale: [
-			{ value: 5, duration: 1000, delay: anime.stagger([0, 300]), easing: 'easeInCubic' }
-		]
-	})
-	
 	
 	
 	// iniciar mas procesos
 	const precarga = new Precarga(showPage);
 	precarga.run();
 
-	const bloque1 = document.getElementById("timeline1");
-	bloque1.idaAniTimeline = aniB1;
-	const ani = new AnimeObserver(bloque1);
-	ani.areaMinima = 0.05;
-	ani.pasos = 20;
-	ani.run();
+	makeTimeline1();
+
+	el.boxTimeline1.idaAniTimeline = animarTimeline1;
+	const observer = new AnimeObserver(el.boxTimeline1);
+	observer.areaMinima = 0.05;
+	observer.pasos = 20;
+	observer.run();
 	
 }
 

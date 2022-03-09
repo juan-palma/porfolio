2594fdd3e077f3febf1f072fa380b5f47d1498e0
Juan Palma <juan.palma@me.com>
2022-03-09 15:12:22 -0600

    bosque v1.0

--- assets/js/owner/main.js
+++ assets/js/owner/main.js
@@ -12,26 +12,46 @@ const el = idagl.elementos;
 
 // ::::::::::::::::: Funciones :::::::::::::::::
 
-function controlTimeLine1(accion){
-	if(accion == 'stop'){
-		if(el.hasOwnProperty('home_hola_parallax')){
-			el.home_hola_parallax.disable();
-		}
-		el.hhola5.style.animation = 'none';
-		el.hhola4.style.animation = 'none';
-		el.hhola3.style.animation = 'none';
-		el.hhola2.style.animation = 'none';
-		el.home_hola_box.style.display = 'none';
-	} else{
-		if(el.hasOwnProperty('home_hola_parallax')){
-			el.home_hola_parallax.enable();
-		}
-		el.home_hola_box.style.display = 'flex';
-		el.hhola5.style.animation = 'hola5 3s cubic-bezier(0.455, 0.03, 0.515, 0.955) 10ms infinite alternate';
-		el.hhola4.style.animation = 'hola4 4.6s linear 10ms infinite';
-		el.hhola3.style.animation = 'hola3 3s cubic-bezier(0.455, 0.03, 0.515, 0.955) 10ms infinite alternate';
-		el.hhola2.style.animation = 'hola2 4.6s linear 10ms infinite';
+function controlTimeLine1(area, accion){
+	switch(area){
+		case 'hola':
+			if(accion == 'stop'){
+				if(el.hasOwnProperty('home_hola_parallax')){
+					el.home_hola_parallax.disable();
+				}
+				el.hhola5.style.animation = 'none';
+				el.hhola4.style.animation = 'none';
+				el.hhola3.style.animation = 'none';
+				el.hhola2.style.animation = 'none';
+				el.home_hola_box.style.display = 'none';
+			} else{
+				if(el.hasOwnProperty('home_hola_parallax')){
+					el.home_hola_parallax.enable();
+				}
+				el.home_hola_box.style.display = 'flex';
+				el.hhola5.style.animation = 'hola5 3s cubic-bezier(0.455, 0.03, 0.515, 0.955) 10ms infinite alternate';
+				el.hhola4.style.animation = 'hola4 4.6s linear 10ms infinite';
+				el.hhola3.style.animation = 'hola3 3s cubic-bezier(0.455, 0.03, 0.515, 0.955) 10ms infinite alternate';
+				el.hhola2.style.animation = 'hola2 4.6s linear 10ms infinite';
+			}
+		break;
+
+		case 'bosque':
+			if(accion == 'stop'){
+				if(el.hasOwnProperty('home_bosque_parallax')){
+					el.home_bosque_parallax.disable();
+				}
+				
+			} else{
+				if(el.hasOwnProperty('home_bosque_parallax')){
+					el.home_bosque_parallax.enable();
+				}
+				
+			}
+		break;
 	}
+
+	
 }
 function animarTimeline1(data){
 	//console.log(data.padrePdesplazado * 10);
@@ -42,12 +62,17 @@ function makeTimeline1(parametro){
 	let targetsHola1 = document.querySelectorAll('#home_hola_box .hhola .anibox');
 	targetsHola1 = [...targetsHola1];
 	targetsHola1.reverse();
+
+	let targetsBosque = document.querySelectorAll('#home_bosque_box .hbosque .anibox');
+	targetsBosque = [...targetsBosque];
+	targetsBosque.reverse();
+
+
 	el.timeline1 = anime.timeline({
 		easing: "linear",
 		autoplay: false,
 	}).add({
 		targets: targetsHola1,
-		duration: 2000,
 		opacity: [
 			{ value: 0, duration: 250, delay: anime.stagger([400, 1300]), easing: 'linear' }
 		],
@@ -55,12 +80,32 @@ function makeTimeline1(parametro){
 			{ value: 8, duration: 2000, delay: anime.stagger([0, 320]), easing: 'linear' }
 		],
 		changeBegin: ()=>{
-			if(el.timeline1.data.sentido == "reverse"){controlTimeLine1('run');};
+			if(el.timeline1.data.sentido == "reverse"){controlTimeLine1('hola', 'run');};
 		},
 		changeComplete: ()=>{
-			if(el.timeline1.data.sentido == "normal"){controlTimeLine1('stop');};
+			if(el.timeline1.data.sentido == "normal"){controlTimeLine1('hola', 'stop');};
 		}
-	})
+	}).set(targetsBosque, {'scale': '0.8', 'translateY':'-14%'})
+	.add({
+		targets: targetsBosque,
+		scale: [
+			{ value: 1, duration: 400, delay: anime.stagger([0, 360]), easing: 'linear' },
+			{ value: 8, duration: 2000, delay: anime.stagger([4300, 4620]), easing: 'linear' }
+		],
+		translateY: [
+			{ value: 0, duration: 400, delay: anime.stagger([0, 360]), easing: 'linear' }
+		],
+		opacity: [
+			{ value: 0, duration: 250, delay: anime.stagger([4500, 5400]), easing: 'linear' }
+		],
+		changeBegin: ()=>{
+			controlTimeLine1('bosque', 'run');
+		},
+		changeComplete: ()=>{
+			console.log('señal');
+			controlTimeLine1('bosque', 'stop');
+		}
+	}, '-=1450')
 }
 
 
@@ -75,8 +120,15 @@ function makeParallaxs(){
 		hoverOnly: false,
 		limitX: window.innerWidth * .088,
 		limitY: window.innerHeight * .088,
-		calibrateX:true
+		calibrateX: true
 	});
+
+	el.home_bosque_parallax = new Parallax(document.getElementById('home_bosque_box'), {
+		relativeInput: false,
+		hoverOnly: false,
+		calibrateX: true
+	});
+	el.home_bosque_parallax.disable();
 }
 function permissionMotion (e, f) {
 	switch(e){

4213b6956b694c84f1e4a75d7cdeae11d4f8d6a6
Juan Palma <juan.palma@me.com>
2022-04-06 21:04:48 -0500

    cambio codigo facebook a main.js

--- assets/js/owner/main.js
+++ assets/js/owner/main.js
@@ -848,6 +848,18 @@ function iniciar() {
 	el.observer = new AnimeObserver(observerArray);
 	el.observer.areaMinima = 0.05;
 	el.observer.pasos = 20;
+
+
+
+	
+	//Código de facebook Chat
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
 	
 }
 
